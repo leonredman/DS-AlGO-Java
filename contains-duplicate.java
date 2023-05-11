cf72d2c036add3e8605fb6516e5291fa106e85ac
leonredman <leonproart@yahoo.com>
2023-05-10 21:57:34 -0700

    added pseudo code solution hints

--- contains-duplicate.java
+++ contains-duplicate.java
@@ -1,5 +1,12 @@
 import java.util.HashSet;
 
+// we can use a HashSet
+// we loop thru the array and check if the HashSet "contains" the value of the array at the index
+// then we can return true
+// otherwise we simply add the value to the HashSet
+// if the values are not present after going through all the elements of the array simply return false
+// Time Complexity is O(n) as we do one loop
+
 class Solution {
   public boolean containsDuplicate(int[] nums) {
       HashSet<Integer> numbers = new HashSet<>();
